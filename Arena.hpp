af676163f6073f03ef75367536f845738fdcc6ff
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-11-02 17:41:09 +0100

    improve logic

--- Arena.hpp
+++ Arena.hpp
@@ -5,10 +5,48 @@ class Champions
 {
 public:
     string name;
-    int attack;
-    int defense;
-    int magic;
-    int luck;
-    Champions(string a, int att, int def, int mag, int luc);
-    ~Champions();
+    int attack, defense, magic, luck;
+    Champions(const string &n, int att, int def, int mag, int luc);
+};
+
+class Arena
+{
+public:
+    vector<Champion> champions;
+
+    void addChampion(const Champion &c)
+    {
+        if (champions.size() < 4)
+        {
+            champions.push_back(c);
+            cout << c.name << " a rejoint l'arene!" << endl;
+        }
+        else
+        {
+            cout << "L'arene est pleine, " << c.name << " ne peut pas rejoindre!" << endl;
+        }
+    }
+
+    void battle()
+    {
+        if (champions.size() < 2)
+        {
+            cout << "Pas assez de champions pour un combat!" << endl;
+            return;
+        }
+
+        int attackerIndex = rand() % champions.size();
+        int defenderIndex;
+
+        do
+        {
+            defenderIndex = rand() % champions.size();
+        } while (attackerIndex == defenderIndex);
+
+        Champion &attacker = champions[attackerIndex];
+        Champion &defender = champions[defenderIndex];
+
+        // Ici, ajoutez la logique pour déterminer le gagnant et affichez les résultats du combat
+        cout << attacker.name << " attaque " << defender.name << "!" << endl;
+    }
 };
--- Functions.cpp
+++ Functions.cpp
@@ -1,15 +1,5 @@
 #include "Arena.hpp"
 #include <iostream>
 
-Champions::Champions(string a, int att, int def, int mag, int luc)
-{
-    name = a;
-    attack = att;
-    defense = def;
-    magic = mag;
-    luck = luc;
-    cout << name << " enter the Arena "
-         << "\n";
-}
-
-Champions::~Champions() {}
+Champions::Champions(const string &n, int a, int d, int m, int l)
+    : name(n), attack(a), defense(d), magic(m), luck(l) {}
