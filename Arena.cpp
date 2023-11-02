d7303e41abba875e1e03ef07169998352c73f42b
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-11-02 17:27:22 +0100

    improve

--- Arena.cpp
+++ Arena.cpp
@@ -3,23 +3,23 @@
 #include <string>
 #include "Arena.hpp"
 #include <cstdlib>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
 int main()
 {
     int N = 20;
+    int i = 0;
     vector<Champions> champions;
 
-    for (int i = 0; i < 5; i++)
+    while (i >= 0)
     {
-        // Création d'un nouveau champion
-        Champions champion("villain" + to_string(i), rand() % N, rand() % N, rand() % N, rand() % N);
+        Champions champion("villain" + to_string(i + 1), rand() % N, rand() % N, rand() % N, rand() % N);
 
-        // Ajout du champion au vecteur
         champions.push_back(champion);
 
-        // Affichage des attributs du champion
         cout << "Champion " << i << ":" << endl;
         cout << "Name: " << champions[i].name << endl
              << "Attack: " << champions[i].attack << endl
@@ -27,5 +27,8 @@ int main()
              << "Magic: " << champions[i].magic << endl
              << "Luck: " << champions[i].luck << endl
              << endl;
+
+        this_thread::sleep_for(chrono::seconds(2));
+        i++;
     }
 }
