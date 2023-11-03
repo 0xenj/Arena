c1900e330ee605e607f3942b058188606b3a0b1e
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-11-03 14:01:56 +0100

    improve

--- Functions.cpp
+++
@@ -1,5 +0,0 @@
-// #include "Arena.hpp"
-// #include <iostream>
-
-// Champions::Champions(const string &n, int a, int d, int m, int l)
-//     : name(n), attack(a), defense(d), magic(m), luck(l) {}
