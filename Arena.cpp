ffcbe3ca80c3ef71bbde6d6a95f71c00983f79fc
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-11-03 12:10:41 +0100

    improve winning condition

--- Arena.cpp
+++ Arena.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include <ctime>
 #include <chrono>
 #include <thread>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,18 +13,29 @@ class Champion
 {
 public:
     string name;
-    int attack, defense, magic, luck, lives;
+    int attack, defense, magic, luck, lives, score;
 
     Champion(const string &n, int a, int d, int m, int l)
-        : name(n), attack(a), defense(d), magic(m), luck(l), lives(3) {}
+        : name(n), attack(a), defense(d), magic(m), luck(l), lives(3), score(0) {}
 
     string getFormattedNameWithAttributes() const
     {
         return name + " [" + to_string(attack) +
-               "," + to_string(defense) +
-               "," + to_string(magic) +
-               "," + to_string(luck) +
-               "](" + to_string(lives) + ")";
+               ", " + to_string(defense) +
+               ", " + to_string(magic) +
+               ", " + to_string(luck) +
+               ", " + to_string(lives) +
+               "](points: " + to_string(score) + ")";
+    }
+
+    void addScore(int points)
+    {
+        score += points;
+    }
+
+    bool hasReachedScoreLimit(int limit) const
+    {
+        return score >= limit;
     }
 };
 
@@ -44,12 +57,12 @@ public:
         }
     }
 
-    void battle()
+    bool battle()
     {
         if (champions.size() < 2)
         {
             cout << "Pas assez de champions pour un combat!" << endl;
-            return;
+            return false;
         }
 
         int attackerIndex = rand() % champions.size();
@@ -65,19 +78,15 @@ public:
         int attackerWins = 0;
         int defenderWins = 0;
 
-        // Confrontation 1: Attaque vs Magie
+        // Confrontations
         if (attacker.attack > defender.magic)
             attackerWins++;
         if (defender.attack > attacker.magic)
             defenderWins++;
-
-        // Confrontation 2: Magie vs Défense
         if (attacker.magic > defender.defense)
             attackerWins++;
         if (defender.magic > attacker.defense)
             defenderWins++;
-
-        // Confrontation 3: Défense vs Attaque
         if (attacker.defense > defender.attack)
             attackerWins++;
         if (defender.defense > attacker.attack)
@@ -97,43 +106,59 @@ public:
             loser = &attacker;
         }
 
-        if (loser && loser->luck > winner->luck)
+        // Surviving by luck
+        if (loser && loser->luck > winner->luck && rand() % 2 == 0)
         {
-            if (rand() % 2 == 0)
-            { // 50% chance
-                cout << loser->getFormattedNameWithAttributes() << " a de la chance et survit avec " << loser->lives << " vies!" << endl;
-                return;
-            }
+            cout << loser->getFormattedNameWithAttributes() << " a de la chance et survit avec " << loser->lives << " vies!" << endl;
+            loser->addScore(5);
+            return false;
         }
 
         if (winner)
         {
             loser->lives--;
+            winner->addScore(10);
             cout << winner->getFormattedNameWithAttributes() << " gagne le combat contre " << loser->getFormattedNameWithAttributes() << ". ";
             cout << loser->getFormattedNameWithAttributes() << " a maintenant " << loser->lives << " vies." << endl;
+
             if (loser->lives <= 0)
             {
                 cout << loser->getFormattedNameWithAttributes() << " est elimine de l'arene!" << endl;
-                champions.erase(champions.begin() + (loser - &champions[0])); // Remove the champion who lost all lives
+                winner->addScore(5);
+                champions.erase(champions.begin() + (loser - &champions[0]));
             }
         }
         else
         {
             cout << "Le combat entre " << attacker.getFormattedNameWithAttributes() << " et " << defender.getFormattedNameWithAttributes() << " se termine par un match nul." << endl;
+            attacker.addScore(5);
+            defender.addScore(5);
         }
+
+        for (const auto &champion : champions)
+        {
+            if (champion.hasReachedScoreLimit(100))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 };
 
-int main()
+void simulateCombat()
 {
-    srand(static_cast<unsigned int>(time(nullptr))); // Initialisation du générateur de nombres aléatoires
+    srand(static_cast<unsigned int>(time(nullptr)));
+
     Arena arena;
     int N = 20;
     int i = 0;
     auto lastChampionTime = chrono::steady_clock::now();
     auto lastBattleTime = chrono::steady_clock::now();
+    bool scoreLimitReached = false;
 
-    while (true)
+    while (!scoreLimitReached)
     {
         auto now = chrono::steady_clock::now();
 
@@ -151,8 +176,19 @@ int main()
             lastBattleTime = now;
         }
 
-        this_thread::sleep_for(chrono::milliseconds(100));
+        scoreLimitReached = arena.battle();
+        this_thread::sleep_for(chrono::seconds(1));
     }
 
+    cout << "The combat has ended. Final Scores:" << endl;
+    for (const auto &champion : arena.champions)
+    {
+        cout << champion.getFormattedNameWithAttributes() << endl;
+    }
+}
+
+int main()
+{
+    simulateCombat();
     return 0;
 }
--- Arena.hpp
+++ Arena.hpp
@@ -1,13 +1,29 @@
-// #include <string>
+// #include <iostream>
 // #include <vector>
+// #include <string>
+// #include <cstdlib>
+// #include <chrono>
+// #include <thread>
+
 // using namespace std;
 
-// class Champions
+// class Champion
 // {
 // public:
 //     string name;
-//     int attack, defense, magic, luck;
-//     Champions(const string &n, int att, int def, int mag, int luc);
+//     int attack, defense, magic, luck, lives;
+
+//     Champion(const string &n, int a, int d, int m, int l)
+//         : name(n), attack(a), defense(d), magic(m), luck(l), lives(3) {}
+
+//     string getFormattedNameWithAttributes() const
+//     {
+//         return name + " [" + to_string(attack) +
+//                "," + to_string(defense) +
+//                "," + to_string(magic) +
+//                "," + to_string(luck) +
+//                "](" + to_string(lives) + ")";
+//     }
 // };
 
 // class Arena
@@ -20,11 +36,11 @@
 //         if (champions.size() < 4)
 //         {
 //             champions.push_back(c);
-//             cout << c.name << " a rejoint l'arene!" << endl;
+//             cout << c.getFormattedNameWithAttributes() << " a rejoint l'arene!" << endl;
 //         }
 //         else
 //         {
-//             cout << "L'arene est pleine, " << c.name << " ne peut pas rejoindre!" << endl;
+//             cout << "L'arene est pleine, " << c.getFormattedNameWithAttributes() << " ne peut pas rejoindre!" << endl;
 //         }
 //     }
 
@@ -38,7 +54,6 @@
 
 //         int attackerIndex = rand() % champions.size();
 //         int defenderIndex;
-
 //         do
 //         {
 //             defenderIndex = rand() % champions.size();
@@ -47,7 +62,97 @@
 //         Champion &attacker = champions[attackerIndex];
 //         Champion &defender = champions[defenderIndex];
 
-//         // Ici, ajoutez la logique pour déterminer le gagnant et affichez les résultats du combat
-//         cout << attacker.name << " attaque " << defender.name << "!" << endl;
+//         int attackerWins = 0;
+//         int defenderWins = 0;
+
+//         // Confrontation 1: Attaque vs Magie
+//         if (attacker.attack > defender.magic)
+//             attackerWins++;
+//         if (defender.attack > attacker.magic)
+//             defenderWins++;
+
+//         // Confrontation 2: Magie vs Défense
+//         if (attacker.magic > defender.defense)
+//             attackerWins++;
+//         if (defender.magic > attacker.defense)
+//             defenderWins++;
+
+//         // Confrontation 3: Défense vs Attaque
+//         if (attacker.defense > defender.attack)
+//             attackerWins++;
+//         if (defender.defense > attacker.attack)
+//             defenderWins++;
+
+//         Champion *winner = nullptr;
+//         Champion *loser = nullptr;
+
+//         if (attackerWins > defenderWins)
+//         {
+//             winner = &attacker;
+//             loser = &defender;
+//         }
+//         else if (defenderWins > attackerWins)
+//         {
+//             winner = &defender;
+//             loser = &attacker;
+//         }
+
+//         if (loser && loser->luck > winner->luck)
+//         {
+//             if (rand() % 2 == 0)
+//             { // 50% chance
+//                 cout << loser->getFormattedNameWithAttributes() << " a de la chance et survit avec " << loser->lives << " vies!" << endl;
+//                 return;
+//             }
+//         }
+
+//         if (winner)
+//         {
+//             loser->lives--;
+//             cout << winner->getFormattedNameWithAttributes() << " gagne le combat contre " << loser->getFormattedNameWithAttributes() << ". ";
+//             cout << loser->getFormattedNameWithAttributes() << " a maintenant " << loser->lives << " vies." << endl;
+//             if (loser->lives <= 0)
+//             {
+//                 cout << loser->getFormattedNameWithAttributes() << " est elimine de l'arene!" << endl;
+//                 champions.erase(champions.begin() + (loser - &champions[0])); // Remove the champion who lost all lives
+//             }
+//         }
+//         else
+//         {
+//             cout << "Le combat entre " << attacker.getFormattedNameWithAttributes() << " et " << defender.getFormattedNameWithAttributes() << " se termine par un match nul." << endl;
+//         }
 //     }
 // };
+
+// int main()
+// {
+//     srand(static_cast<unsigned int>(time(nullptr)));
+//     Arena arena;
+//     int N = 20;
+//     int i = 0;
+//     auto lastChampionTime = chrono::steady_clock::now();
+//     auto lastBattleTime = chrono::steady_clock::now();
+
+//     while (true)
+//     {
+//         auto now = chrono::steady_clock::now();
+
+//         if (chrono::duration_cast<chrono::seconds>(now - lastChampionTime).count() >= 5)
+//         {
+//             Champion newChampion("villain" + to_string(i), rand() % N, rand() % N, rand() % N, rand() % N);
+//             arena.addChampion(newChampion);
+//             lastChampionTime = now;
+//             i++;
+//         }
+
+//         if (chrono::duration_cast<chrono::seconds>(now - lastBattleTime).count() >= 1)
+//         {
+//             arena.battle();
+//             lastBattleTime = now;
+//         }
+
+//         this_thread::sleep_for(chrono::milliseconds(100));
+//     }
+
+//     return 0;
+// }
